de09a82427940a89af7a7effb71f291e795802a6
Kanchanop6509650229 <kanchanop.bua@dome.tu.ac.th>
2025-02-15 09:59:23 +0700

    Cleanup migration files and update .gitignore for local environment settings

--- prisma/migrations/20250213164746_npx_prisma_db_seed/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `current` on the `career` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `career` DROP COLUMN `current`;
